57215b1f84c8441f7763d50055d212546ce19ac2
olanrewaju <mubbyone@gmail.com>
2024-02-23 22:01:20 +0100

    updating new features

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,36 +17,16 @@
     }
 
 
+    var sidebar = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidebar.style.right = "0";
+}
+
+function closeMenu(){
+    sidebar.style.right = "-200px"
+}
 
 
-    // <div id="contacts">
-    //   <div class="container">
-    //     <div class="row">
-    //         <div class="contact-left">
-    //             <h1 class="sub-title">Contact Me</h1>
-    //             <p><i class="uil uil-message"></i>contact@example.com</p>
-    //             <p><i class="uil uil-calling"></i>0905-382-1103</p>
-    //         </div>
-    //         <div class="social-icons">
-    //             <a href="https://www.facebook.com"><i class="uil uil-facebook-f"></i></a>
-    //             <a href=""><i class="uil uil-linkedin"></i></a>
-    //             <a href=""><i class="uil uil-twitter-alt"></i></a>
-    //             <a href=""><i class="uil uil-twitter-alt"></i></a>
-    //         </div>
-    //         <a href="" class="btn  btn2">Download CV</a>
-    //       </div>
-    //       <div class="contact-right">
-    //         <form>
-    //           <input type="text" name="Name" placeholder="Your name" required />
-    //           <input type="text" name="Email" placeholder="Your email" required />
-    //           <textarea
-    //             name="Message"
-    //             rows="6"
-    //             placeholder="Your message"
-    //           ></textarea>
-    //           <button type="submit" name="submit">Submit</button>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+document.getElementById("openButton").addEventListener("click", openMenu);
+document.getElementById("closeButton").addEventListener("click", closeMenu);
